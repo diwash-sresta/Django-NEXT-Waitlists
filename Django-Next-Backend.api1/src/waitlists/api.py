ce9c8ce6ad9e66b0300c3ca3c0e88399f030771e
Diwash <diwashshrestha108@gmail.com>
2025-08-01 21:03:31 +0545

    added post method for waitlist entry

--- Django-Next-Backend.api1/src/waitlists/api.py
+++ Django-Next-Backend.api1/src/waitlists/api.py
@@ -1,16 +1,24 @@
 from ninja import Router
 from typing import List
 from .models import WaitlistEntry
-from .schemas import WaitlistEntryListSchema, WaitlistEntryDetailSchema
+from .schemas import WaitlistEntryListSchema, WaitlistEntryDetailSchema, WaitlistEntryCreateSchema
 from django.shortcuts import get_object_or_404
 from ninja_jwt.authentication import JWTAuth
 
 router = Router()
-
+#/api/waitlists/
 @router.get("", response=List[WaitlistEntryListSchema], auth =JWTAuth() )
 def list_waitlist_entries(request):
   qs = WaitlistEntry.objects.all()
   return qs
+
+#/api/waitlists/
+@router.post("",response=WaitlistEntryDetailSchema)
+def create_waitlist_entry(request,data:WaitlistEntryCreateSchema):
+  obj = WaitlistEntry.objects.create(**data.dict())
+  print(request.user)
+  return obj
+
 
 @router.get("{entry_id}/", response=WaitlistEntryDetailSchema,auth =JWTAuth())
 def get_waitlist_entry(request,entry_id:int):
